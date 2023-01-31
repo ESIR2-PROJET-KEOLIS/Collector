7735a19f7cd9086152ed959a27320e005deaba85
Payll <yann.paillard@laposte.net>
2023-01-31 10:25:52 +0100

    create Dev and prod mod, Fix connection to rabbitmq

--- appCollector/AppCollector.py
+++ appCollector/AppCollector.py
@@ -9,16 +9,27 @@ import pika
 def setup_logger():
     log.remove()
     log.add(stdout, format="{time} {level} {message}", level="INFO")
-    log.add(f"../logs/{argv[1]}Collector.log", format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+    log.add(f"../logs/{argv[1]}Collector.log",
+            format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
             rotation="1 week",
             retention="1 month", level="DEBUG")
 
 
 def main(name, collector, time=-1):
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='rabbitmq'))
-    channel = connection.channel()
-    channel.queue_declare(queue=name, durable=True)
+    log.info("create rabbitmq queue")
+    isNotConnected = True
+    while isNotConnected:
+        try:
+            connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+            channel = connection.channel()
+            channel.queue_declare(queue=name, durable=True)
+            isNotConnected = False
+        except Exception as e:
+            log.error(e)
+            log.info("Waiting for rabbitmq to be ready")
+            sleep(1)
+
+
     log.info(f"Starting appCollector : {name} with {time}s interval")
     while True:
         # Get Data API with request
@@ -38,7 +49,8 @@ def verifArg(tab_argv):
     collectorType = ["REQUEST", "CURL"]
     if len(tab_argv) < 4 or len(tab_argv) > 5:
         log.error("Wrong number of arguments")
-        log.info("Usage : python3 AppCollector.py <name> <type> <url> <time> or python3 AppCollector.py <name> <url>")
+        log.info(
+            "Usage : python3 AppCollector.py <name> <type> <url> <time> or python3 AppCollector.py <name> <url>")
         log.info(
             "Example : python3 AppCollector.py weather REQUEST https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=1234567890 60")
         log.info("help : python3 AppCollector.py help")
