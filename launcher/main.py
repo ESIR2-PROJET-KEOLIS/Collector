1c05680f9f56e7ccacd28cf3fc3a1fb78f8ce3d2
Payll <yann.paillard@laposte.net>
2023-02-14 10:14:44 +0100

    add jsonconfig

--- launcher/main.py
+++ launcher/main.py
@@ -1,12 +1,13 @@
+import json
 import os
 import threading
-import time
 from sys import stdout
 from loguru import logger as log
 
 log.remove()
 log.add(stdout, format="{time} {level} {message}", level="INFO")
-log.add("../logs/appCollector.log", format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}", rotation="1 week",
+log.add("../logs/appCollector.log", format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+        rotation="1 week",
         retention="1 month", level="DEBUG")
 
 
@@ -14,7 +15,7 @@ log.add("../logs/appCollector.log", format="{time:YYYY-MM-DD at HH:mm:ss} | {lev
 
 
 def thread_function(name, type, url, time):
-    print("Thread %s: starting", name)
+    log.info("Thread %s: starting", name)
     PROD = os.environ.get('PROD')
     if PROD == "True":
         log.info("PROD")
@@ -28,24 +29,30 @@ def thread_function(name, type, url, time):
         log.info(TheCommand)
 
     os.system(TheCommand)
-    print("Thread %s: finishing", name)
+    log.info("Thread %s: finishing", name)
 
 
 def create_threads():
-    threads = []
-    time = [10]
-    name = ['PositionAllBus']
-    type = ['REQUEST']
-    url = [
-        "\"https://data.explore.star.fr/api/records/1.0/search/?dataset=tco-bus-vehicules-position-tr&q=&rows=1000\""]
-    for key, i in enumerate(time):
-        threads.append(threading.Thread(target=thread_function, args=(name[key], type[key], url[key], i,)))
+    PROD = os.environ.get('PROD')
+    if PROD == "True":
+        path = "./launcher/"
+    else:
+        path = ""
 
+    threads = []
+    with open(path + "config.json", "r") as f:
+        data = json.load(f)
+        for collector in data["collectors"]:
+            log.info(collector["name"], collector["type"], collector["url"], collector["time"])
+            threads.append(threading.Thread(target=thread_function, args=(
+                collector["name"],
+                collector["type"],
+                collector["url"],
+                collector["time"],)))
     return threads
 
 
 if __name__ == '__main__':
-    log.info("Initial Sleep")
     log.info("Starting appCollector")
     threads = create_threads()
     for thread in threads:
